5e596e3c14923e7be2d3589e4d1e081d95504609
felipe flores valdivia <felipe flores valdivia>
2016-10-19 15:09:26 -0300

    Git ignore and db configuration

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  database : 'sys',
+  database : 'ada',
   password : 'admin'
 });
 
